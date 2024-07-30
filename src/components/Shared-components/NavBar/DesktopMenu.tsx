535aa8d608bab425e231a85a88589a4447bb33b8
alireza <alirezanghngh1379@gmail.com>
2024-07-30 15:22:33 +0430

    feat: -Add privacy&policy page -Address correct link to Privacy Page

--- src/components/Shared-components/NavBar/DesktopMenu.tsx
+++ src/components/Shared-components/NavBar/DesktopMenu.tsx
@@ -67,7 +67,7 @@ function DesktopMenu({
             <NavItem targetLink="/categories" label="وبلاگ" />
             <NavItem targetLink="/contact-us" label="تماس با ما" />
             <NavItem targetLink="/about-us" label="درباره ما" />
-            <NavItem targetLink="/categories" label="قوانین" />
+            <NavItem targetLink="/policy" label="قوانین" />
             {userLoading ? (
               <Loader loadingCondition={userLoading} />
             ) : user ? (
