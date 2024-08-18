a795ef7b80cca602e2998e96d09fb040111d14dc
alireza <alirezanghngh1379@gmail.com>
2024-08-18 09:52:55 +0430

    Feat: -Handle create offer code form -set client schema for offer code form

--- src/components/layout-components/AdminPanel/subRoutes/Offers/OfferForm.tsx
+++ src/components/layout-components/AdminPanel/subRoutes/Offers/OfferForm.tsx
@@ -1,17 +1,41 @@
 "use client";
 import MainBtn from "@/components/UI/Buttons/MainBtn";
+import Loader from "@/components/UI/loader/Loader";
 import MainTextField from "@/components/UI/TextFiels/MainTextField";
-import React from "react";
+import useCreateOfferCode from "@/hooks/offers/useCreateOfferCode";
+import { OfferModelType } from "@/types/models/offers.type";
 import { useForm } from "react-hook-form";
 
 function OfferForm() {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors, isValid, dirtyFields },
-  } = useForm();
+  } = useForm<OfferModelType>();
+  const { addNewOffCode, isCreateLoading } = useCreateOfferCode();
+  const offerCodeHandler = async (data: OfferModelType) => {
+    try {
+      const { code, maxUsage, percent } = data;
+      await addNewOffCode(
+        {
+          data: { code, maxUsage: Number(maxUsage), percent: Number(percent) },
+        },
+        {
+          onSuccess: () => {
+            reset();
+          },
+        }
+      );
+    } catch (error: any) {
+      console.log(error?.response?.data?.message);
+    }
+  };
   return (
-    <form className="flex flex-col justify-between items-center mt-8 p-6 border border-main_brown">
+    <form
+      onSubmit={handleSubmit(offerCodeHandler)}
+      className="flex flex-col justify-between items-center mt-8 p-6 border border-main_brown"
+    >
       <div className="flex sm:flex-row flex-col gap-y-4 sm:gap-y-0 w-full child:w-full gap-x-6  items-center">
         <MainTextField
           type="text"
@@ -22,6 +46,16 @@ function OfferForm() {
           variant="borderFill"
           label="کد تخفیف"
           className="w-full border-main_brown"
+          validattionschema={{
+            required: {
+              value: true,
+              message: "این فیلد نمی تواند خالی باشد",
+            },
+            maxLength: {
+              value: 50,
+              message: "حداکثر ۵۰ کاراکتر",
+            },
+          }}
         />
         <MainTextField
           type="number"
@@ -31,7 +65,22 @@ function OfferForm() {
           name="percent"
           variant="borderFill"
           label="درصد تخفیف"
-          className="w-full border-main_brown"
+          className="w-full
+           border-main_brown"
+          validattionschema={{
+            required: {
+              value: true,
+              message: "این فیلد نمی تواند خالی باشد",
+            },
+            min: {
+              value: 5,
+              message: "حداقل ۵ درصد",
+            },
+            max: {
+              value: 100,
+              message: "حداکثر ۱۰۰ درصد",
+            },
+          }}
         />
       </div>
       <div className="w-full mt-4  gap-x-6 flex sm:flex-row  flex-col gap-y-4 sm:gap-y-0 ">
@@ -44,12 +93,33 @@ function OfferForm() {
             name="maxUsage"
             variant="borderFill"
             label="تعداد مجاز استفاده"
+            validattionschema={{
+              required: {
+                value: true,
+                message: "این فیلد نمی تواند خالی باشد",
+              },
+            }}
             className=" border-main_brown !w-full "
           />
         </div>
 
-        <MainBtn size="medium" className="mr-auto mt-auto w-full sm:w-[190px]">
-          ایجاد کد
+        <MainBtn
+          type="submit"
+          disabled={!isValid || !Object.keys(dirtyFields).length}
+          size="medium"
+          className={`
+            mr-auto mt-auto w-full sm:w-[190px]
+            ${
+              !isValid || !Object.keys(dirtyFields).length
+                ? "opacity-50"
+                : "opacity-100"
+            }`}
+        >
+          {isCreateLoading ? (
+            <Loader loadingCondition={isCreateLoading} />
+          ) : (
+            "ایجاد کد"
+          )}
         </MainBtn>
       </div>
     </form>
