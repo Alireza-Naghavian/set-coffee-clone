ac0007aab386cd1ef81fa8ec526d8174db2e870a
alireza <alirezanghngh1379@gmail.com>
2024-10-01 22:11:02 +0330

    Feat: -Add minfest.json and defferent size of icons

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,7 @@ const nextConfig = {
       {
         protocol: `https`,
         hostname: `set-coffee.com`,
+        pathname: "/wp-content/**",
       },
     ],
     deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
